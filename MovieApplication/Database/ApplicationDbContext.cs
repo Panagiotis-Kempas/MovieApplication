fdbdb40d5cf8c84f9e751e10915e1297dfcff417
Panos1996 <panos2171996@gmail.com>
2022-04-29 09:13:06 +0300

    Entity Framework Core

---
+++ MovieApplication/Database/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MovieApplication.Models.Entities;
+
+namespace MovieApplication.Database
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<Actor> Actors { get; set; }
+        public DbSet<Director> Directors { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+        public DbSet<Movie> Movies{ get; set; }
+        public DbSet<Photo> Photos { get; set; }
+
+    }
+}

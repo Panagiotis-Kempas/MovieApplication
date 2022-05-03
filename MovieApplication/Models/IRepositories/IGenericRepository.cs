7bc9c5330d87aea50601b1ccd3b232b9952e24f7
Panos1996 <panos2171996@gmail.com>
2022-05-03 23:37:20 +0300

    Repository

---
+++ MovieApplication/Models/IRepositories/IGenericRepository.cs
@@ -0,0 +1,14 @@
+﻿namespace MovieApplication.Models.IRepositories
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        Task<IEnumerable<T>> All();
+
+        Task<T> GetByiD(Guid id);
+
+        Task<bool> Add(T entity);
+
+        Task<bool> Update(T entity);
+        Task<bool> Delete(Guid id);
+    }
+}

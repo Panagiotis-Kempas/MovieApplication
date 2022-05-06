1647ccbe41c0b1be6ac88611bf4dc2600e57875c
Panos1996 <panos2171996@gmail.com>
2022-05-06 22:05:33 +0300

    First Movie

---
+++ MovieApplication/Controllers/MovieController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MovieApplication.Persistence;
+
+namespace MovieApplication.Controllers
+{
+    public class MovieController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        private readonly IUnitOfWork _unitOfWork;
+
+        public MovieController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public IActionResult TopRatedMovies()
+        {
+            var movies = _unitOfWork.Movies.GetHighestRatingMovies();
+            return View(movies);
+        }
+    }
+}
